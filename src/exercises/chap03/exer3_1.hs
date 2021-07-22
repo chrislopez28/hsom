af693ebe92658312373853b2ac93f4e605ffe49c
Chris Lopez <chrislopez28@gmail.com>
2021-07-21 22:44:31 -0700

    Updated test  melody for exer3_1

--- src/exercises/chap03/exer3_1.hs
+++ src/exercises/chap03/exer3_1.hs
@@ -23,7 +23,17 @@ notes :: [Pitch]
 notes = [(A, 4), (B, 5), (C, 4), (D, 4), (E, 4), (F, 4), (C, 5), (B, 4)]
 
 melody :: [Music Pitch]
-melody = g1 ((f1 2 notes) ++ (f1 4 notes) ++ (f1 (-2) notes) ++ (f1 0 notes)) 
+melody = g1 ((f1 0 notes) ++ (f1 2 notes) ++ (f1 4 notes) ++ (f1 (-2) notes)) 
+
+melody2 :: [Music Pitch]
+melody2 = g1 ((f1 4 notes) ++ (f1 6 notes) ++ (f1 (0) notes) ++ (f1 2 notes)) 
+
+melody3 :: [Music Pitch]
+melody3 = g1 ((f1 8 notes) ++ (f1 10 notes) ++ (f1 (-4) notes) ++ (f1 6 notes)) 
 
 main :: IO ()
-main = play $ line (melody ++ (f4 melody) ++ (f4 (f4 melody)) ++ (f4 (f4 melody)) ++ melody ++ [Prim (Note hn (B, 4)), Prim (Note hn (C, 5))])
+main = play $ chord [line (melody ++ (f4 melody) ++ (f4 (f4 melody)) ++ (f4 (f4 melody)) ++ melody ++ [Prim (Note hn (B, 4)), Prim (Note hn (C, 5))]), 
+                     line (melody ++ melody ++ melody ++ [Prim (Note hn (D, 5)), Prim (Note hn (E, 5))]),
+                     line (melody2 ++ melody2 ++ melody2 ++ [Prim (Note hn (F, 5)), Prim (Note hn (G, 5))]),
+                     line (melody3 ++ melody3 ++ melody3 ++ [Prim (Note hn (A, 5)), Prim (Note hn (C, 4))])
+                     ]
