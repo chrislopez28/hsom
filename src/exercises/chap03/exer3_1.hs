3a0be0ea54566cdfc40b4bccefb06fae5d871de6
Chris Lopez <chrislopez28@gmail.com>
2021-07-21 22:18:45 -0700

    Finished solution to exercise 3.1

--- src/exercises/chap03/exer3_1.hs
+++ src/exercises/chap03/exer3_1.hs
@@ -5,12 +5,25 @@ f1 i p = let f p1 = trans i p1
          in map f p
 
 f2 :: [Dur] -> [Music a]
+f2 d = map rest d
 
 f3 :: [Music Pitch] -> [Music Pitch]
+f3 mps = let f (Prim (Note d p)) = note (d/2) p :+: rest (d/2)
+       in map f mps
+
+f4 :: [Music Pitch] -> [Music Pitch]
+f4 mps = let f (Prim (Note d p)) = note (d/2) p
+       in map f mps
 
 g1 :: [Pitch] -> [Music Pitch]
 g1 p = let g p1 = note en p1
        in map g p 
 
+notes :: [Pitch]
+notes = [(A, 4), (B, 5), (C, 4), (D, 4), (E, 4), (F, 4), (C, 5), (B, 4)]
+
+melody :: [Music Pitch]
+melody = g1 ((f1 2 notes) ++ (f1 4 notes) ++ (f1 (-2) notes) ++ (f1 0 notes)) 
+
 main :: IO ()
-main = play $ line (g1 (f1 2 [(A, 4), (B, 5), (C, 4), (D, 4), (E, 4), (F, 4), (C, 5)]))
+main = play $ line (melody ++ (f4 melody) ++ (f4 (f4 melody)) ++ (f4 (f4 melody)) ++ melody ++ [Prim (Note hn (B, 4)), Prim (Note hn (C, 5))])
